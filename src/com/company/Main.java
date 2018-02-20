057cb54a4a973138b8e6d9e3ab63c8eb6d76abfb
Mantas <mantas.muzzz@gmail.com>
2018-02-20 09:34:13 +0200

    padariau

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
+        String name;
+        Scanner in = new Scanner(System.in);
 
+        System.out.println("Iveskite varda");
+        name = in.nextLine();
+
+        for (int i = 0; i < 5; i ++){
+            System.out.println(name);
+        }
     }
 }
